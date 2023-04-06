207364ebf9cabf22e7df54c40a82b1cfcd05519c
kothari1998 <rajankothari98@gmail.com>
2023-04-06 10:50:16 +0530

    add

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# eastvantage_react_test
+# Getting Started with Create React App
+
+npx create-react-app my-app
+
+npm install react-scripts@latest
+
+#run command
+npm start
+
+#axios intsall
+npm i axios
---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = ()=>{
+  //use state
+  const [data, setData] = useState<any[]>([])
+  useEffect(() => {
+    fetchData();
+  });
+
+  //get the name and email
+  const fetchData = async () =>{
+    try {
+      const {data: response} = await axios.get('https://randomuser.me/api');
+      //data
+      setData(response.results);
+      console.log(data);
+      
+      //save data in localStorage
+      localStorage.setItem('items', JSON.stringify(data));
+    } catch (error) {
+      console.error();
+    }
+  }
+
+  const refreshPage = ()=>{
+    // window.location.reload(false);
+    fetchData();
+  }
+ 
+  return(
+    
+    <table className="table">
+        <thead>
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Email Address</th>
+             </tr>
+        </thead>
+        <tbody>
+        {
+            data.map((item, index)=>{
+                return(
+                    <tr key={index}>
+                        <td>{index+1}</td>
+                        <td>{item.name.first}</td>
+                        <td>{item.email}</td>
+                     </tr>
+                )
+            })
+        }
+        </tbody>
+        <div>
+       <button onClick={refreshPage}>Refresh</button>
+    </div>
+    </table>
+    
+)
+     
+};
+
+
+export default App;
